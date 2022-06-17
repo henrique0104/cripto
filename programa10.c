e503f30e33a9cb0f427768f8c0f14cdc8fd55d7f
henrique <107253367+henrique0104@users.noreply.github.com>
2022-06-17 18:59:48 -0300

    Create programa10.c

---
+++ programa10.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main (void){
+
+    int a,p;
+    int i;
+
+    puts("INSIRA a");
+    scanf("%d",&a);
+
+    puts("INSIRA p");
+    scanf("%d",&p);
+
+    if (p%a == 0){
+        printf("não existem inversos, %d = 0 mod %d",a,p);
+        return 1;
+    }
+
+    int mods = 1;
+
+    for (i=1; i<(p-1); i++){
+        mods = (mods * a) % p;
+    }
+
+    printf("o inverso é %d", mods);
+
+    return 0;
+}
